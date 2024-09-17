caf88737afd0828526eab4c83bcc9307ad8c8678
Brian Kovo <23661488+BrianKo14@users.noreply.github.com>
2024-09-16 21:22:13 -0300

    Update MapSumaUno.java

--- Taller03-JML/src/main/java/inge2/dataflow/MapSumaUno.java
+++ Taller03-JML/src/main/java/inge2/dataflow/MapSumaUno.java
@@ -4,14 +4,13 @@ public class MapSumaUno {
 
     // Suma uno a todos los elementos de un array.
     //
-    //@ modifies arr[*];
-    //@ requires true;
-    //@ ensures (\forall int i; 0 <= i && i < arr.length; arr[i] == \old(arr[i] + 1));
-    //@ ensures arr.length == \old(arr.length);
+    //@ requires (\forall int j; 0 <= j < arr.length; arr[j] < Integer.MAX_VALUE);
+    //@ ensures (\forall int j; 0 <= j < arr.length; arr[j] == \old(arr[j])+1);
     public static void mapSumaUno(int[] arr) {
         
-        //@ maintaining 0 <= i && i <= arr.length;
-        //@ maintaining (\forall int j; 0 <= j && j < i; arr[j] == \old(arr[j] + 1));
+        //@ maintaining 0 <= i <= arr.length;
+        //@ maintaining (\forall int j; i <= j < arr.length; arr[j] == \old[arr[j]])
+        //@ maintaining (\forall int j; 0 <= j < i; arr[j] == \old(arr[j])+1);
         //@ decreases arr.length - i;
         for (int i = 0; i < arr.length; i++) {
             arr[i] = arr[i] + 1;
