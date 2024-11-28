ae31c8d6fe40c19a05df4773de2a2a44394cb28f
BrianKo14 <23661488+BrianKo14@users.noreply.github.com>
2024-11-27 22:41:30 -0300

    Taller 6: ej 1

--- Taller06-CgiDecode/test/test_cgi_decode.py
+++ Taller06-CgiDecode/test/test_cgi_decode.py
@@ -4,9 +4,31 @@ from src.cgi_decode import cgi_decode
 
 
 class TestCgiDecode(unittest.TestCase):
-    def testExample(self):
-        # TODO COMPLETAR
-        cgi_decode("Hello World")
-        self.assertTrue(True)
-        self.assertFalse(False)
-        self.assertEqual(True, False)
+
+    # Palabra vacía
+    def testEmpty(self):
+        self.assertEqual(cgi_decode(""), "")
+
+    # Sin codificación
+    def testNoEncoding(self):
+        self.assertEqual(cgi_decode("Hello World"), "Hello World")
+
+    # Con +
+    def testPlus(self):
+        self.assertEqual(cgi_decode("Hello+World"), "Hello World")
+
+    # Con %
+    def testPercent(self):
+        self.assertEqual(cgi_decode("Hello%20World"), "Hello World")
+
+    # Digit low no es un dígito hexadecimal
+    def testInvalidDigitLow(self):
+        with self.assertRaises(ValueError):
+            cgi_decode("Hello%2GWorld")
+
+    # Digit high no es un dígito hexadecimal
+    def testInvalidDigitHigh(self):
+        with self.assertRaises(ValueError):
+            cgi_decode("Hello%G2World")
+
+    
