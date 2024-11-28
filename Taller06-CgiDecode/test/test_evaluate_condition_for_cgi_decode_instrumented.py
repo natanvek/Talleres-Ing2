c0bf44e5b5d10b0d59f1ddde62498ffb09b8abd9
natanvek <50033520+natanvek@users.noreply.github.com>
2024-11-28 17:09:35 -0300

    Taller6 ej3 terminado

--- Taller06-CgiDecode/test/test_evaluate_condition_for_cgi_decode_instrumented.py
+++ Taller06-CgiDecode/test/test_evaluate_condition_for_cgi_decode_instrumented.py
@@ -3,89 +3,103 @@ import unittest
 from src.cgi_decode_instrumented import cgi_decode_instrumented
 from src.evaluate_condition import clear_maps, get_true_distance, get_false_distance
 
+def distancia_palabra_operador(palabra, operador):
+    return min((abs(ord(x) - ord(operador)) for x in palabra))
 
 class TestEvaluateConditionForCgiDecodeInstrumented(unittest.TestCase):
-
-    # def test_example(self):
-    #     clear_maps()
-    #     result = cgi_decode_instrumented("Hello+World")
-
-    #     self.assertEqual(get_true_distance(1), 0)
-    #     self.assertEqual(get_true_distance(2), 0)
-    #     self.assertEqual(get_true_distance(3), 35)
-
-    #     self.assertEqual(get_false_distance(1), 0)
-    #     self.assertEqual(get_false_distance(2), 0)
-    #     self.assertEqual(get_false_distance(3), 0)
-
-    #     self.assertEqual(result, "Hello World")
-
-    # def test_not_c1(self):
-    #     clear_maps()
-    #     result = cgi_decode_instrumented("")
-
-    #     self.assertEqual(get_true_distance(1), 1)
-    #     self.assertEqual(get_false_distance(1), 0)
-
-    #     self.assertEqual(result, "")
-
-    # def test_not_eq(self):
-    #     clear_maps()
-    #     result = cgi_decode_instrumented("a")
-
-    #     self.assertEqual(get_true_distance(2), 54)
-    #     self.assertEqual(get_false_distance(2), 0)
-
-    #     self.assertEqual(result, "a")
-
-    # def test_eq(self):
-    #     clear_maps()
-    #     result = cgi_decode_instrumented("+")
-
-    #     self.assertEqual(get_true_distance(2), 0)
-    #     self.assertEqual(get_false_distance(2), 1)
-
-    #     self.assertEqual(result, " ")
-
-    # def test_in(self):
-    #     clear_maps()
-    #     result = cgi_decode_instrumented("%41")
-
-    #     self.assertEqual(get_true_distance(3), 0)
-    #     self.assertEqual(get_false_distance(3), 1)
-
-    #     self.assertEqual(result, "A")
-
-    # def test_not_in(self):
-    #     clear_maps()
-    #     with self.assertRaises(ValueError):
-    #         cgi_decode_instrumented("%4x")
-
-    #     self.assertEqual(get_true_distance(4), 0)
-    #     self.assertEqual(get_false_distance(4), 0)
-
 
     # Palabra vacía
     def testEmpty(self):
         clear_maps()
         self.assertEqual(cgi_decode_instrumented(""), "")
-        self.assertEqual(get_true_distance(1), 2)
+        self.assertEqual(get_true_distance(1), 1)
         self.assertEqual(get_false_distance(1), 0)
+        
+        self.assertIsNone(get_true_distance(2))
+        self.assertIsNone(get_false_distance(2))
+
+        self.assertIsNone(get_true_distance(3))
+        self.assertIsNone(get_false_distance(3))
+
+        self.assertIsNone(get_true_distance(4))
+        self.assertIsNone(get_false_distance(4))
+
+        self.assertIsNone(get_true_distance(5))
+        self.assertIsNone(get_false_distance(5))
+
+        self.assertIsNone(get_true_distance(6))
+        self.assertIsNone(get_false_distance(6))
 
     # Sin codificación
     def testNoEncoding(self):
         clear_maps()
+
         self.assertEqual(cgi_decode_instrumented("Hello World"), "Hello World")
+        self.assertEqual(get_true_distance(1), 0)
+        self.assertEqual(get_false_distance(1), 0)
+        
+        self.assertEqual(get_true_distance(2), 11)
+        self.assertEqual(get_false_distance(2), 0)
+
+        self.assertEqual(get_true_distance(3), 5)
+        self.assertEqual(get_false_distance(3), 0)
+
+        self.assertIsNone(get_true_distance(4))
+        self.assertIsNone(get_false_distance(4))
+
+        self.assertIsNone(get_true_distance(5))
+        self.assertIsNone(get_false_distance(5))
+
+        self.assertIsNone(get_true_distance(6))
+        self.assertIsNone(get_false_distance(6))
+        
+
 
     # Con +
     def testPlus(self):
         clear_maps()
+
         self.assertEqual(cgi_decode_instrumented("Hello+World"), "Hello World")
+        self.assertEqual(get_true_distance(1), 0)
+        self.assertEqual(get_false_distance(1), 0)
+        
+        self.assertEqual(get_true_distance(2), 0)
+        self.assertEqual(get_false_distance(2), 0)
+
+        self.assertEqual(get_true_distance(3), 35)
+        self.assertEqual(get_false_distance(3), 0)
+
+        self.assertIsNone(get_true_distance(4))
+        self.assertIsNone(get_false_distance(4))
+
+        self.assertIsNone(get_true_distance(5))
+        self.assertIsNone(get_false_distance(5))
+
+        self.assertIsNone(get_true_distance(6))
+        self.assertIsNone(get_false_distance(6))
 
     # Con %
     def testPercent(self):
         clear_maps()
         self.assertEqual(cgi_decode_instrumented("Hello%20World"), "Hello World")
+
+        self.assertEqual(get_true_distance(1), 0)
+        self.assertEqual(get_false_distance(1), 0)
+        
+        self.assertEqual(get_true_distance(2), 6)
+        self.assertEqual(get_false_distance(2), 0)
+
+        self.assertEqual(get_true_distance(3), 0)
+        self.assertEqual(get_false_distance(3), 0)
+
+        self.assertEqual(get_true_distance(4), 0)
+        self.assertEqual(get_false_distance(4), 1)
+
+        self.assertEqual(get_true_distance(5), 0)
+        self.assertEqual(get_false_distance(5), 1)
+
+        self.assertIsNone(get_true_distance(6))
+        self.assertIsNone(get_false_distance(6))
 
     # Digit low no es un dígito hexadecimal
     def testInvalidDigitLow(self):
@@ -93,10 +107,47 @@ class TestEvaluateConditionForCgiDecodeInstrumented(unittest.TestCase):
         with self.assertRaises(ValueError):
             cgi_decode_instrumented("Hello%2GWorld")
 
+        self.assertEqual(get_true_distance(1), 0)
+        self.assertEqual(get_false_distance(1), 8)
+        
+        self.assertEqual(get_true_distance(2),6 )
+        self.assertEqual(get_false_distance(2),0 ) 
+
+        self.assertEqual(get_true_distance(3), 0)
+        self.assertEqual(get_false_distance(3), 0)
+
+        self.assertEqual(get_true_distance(4), 0)
+        self.assertEqual(get_false_distance(4), 1)
+
+        self.assertEqual(get_true_distance(5), 1)
+        self.assertEqual(get_false_distance(5), 0)
+
+        self.assertIsNone(get_true_distance(6))
+        self.assertIsNone(get_false_distance(6))
+
+
     # Digit high no es un dígito hexadecimal
     def testInvalidDigitHigh(self):
         clear_maps()
         with self.assertRaises(ValueError):
             cgi_decode_instrumented("Hello%G2World")
 
+
+        self.assertEqual(get_true_distance(1), 0)
+        self.assertEqual(get_false_distance(1), 8)
+        
+        self.assertEqual(get_true_distance(2), 6)
+        self.assertEqual(get_false_distance(2), 0) 
+
+        self.assertEqual(get_true_distance(3), 0)
+        self.assertEqual(get_false_distance(3), 0)
+
+        self.assertEqual(get_true_distance(4), 1)
+        self.assertEqual(get_false_distance(4), 0)
+
+        self.assertIsNone(get_true_distance(5))
+        self.assertIsNone(get_false_distance(5))
+
+        self.assertIsNone(get_true_distance(6))
+        self.assertIsNone(get_false_distance(6))
     
